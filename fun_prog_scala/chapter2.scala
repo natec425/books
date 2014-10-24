3ac87440a2257098837b77bda83418c4ae5ffe10
Nate Clark <natec425@gmail.com>
2014-10-23 21:38:02 -0500

    Added exercise uncurry (2.4)

--- fun_prog_scala/chapter2.scala
+++ fun_prog_scala/chapter2.scala
@@ -17,4 +17,7 @@ object chapter2 {
 
   def curry[A, B, C](f: (A, B) => C) : A => (B => C) = 
     a => b => f(a, b)
+
+  def uncurry[A, B, C](f: A => B => C) : (A, B) => C =
+    (a, b) => f(a)(b)
 }
