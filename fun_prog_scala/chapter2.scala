27d6d5a6edb281c52a50602b15c17706fbb10baa
Nate Clark <natec425@gmail.com>
2014-10-23 19:18:24 -0500

    Added exercises fib and isSorted (2.1, 2.2)

---
+++ fun_prog_scala/chapter2.scala
@@ -0,0 +1,17 @@
+object chapter2 {
+  def fib(n: Int) : Int = {
+    def tr_fib(i: Int, j: Int, iter: Int) : Int = 
+      if (iter == n) i else tr_fib(j, i+j, iter+1)
+
+    tr_fib(0, 1, 0)
+  }
+
+  def isSorted[A](as: Array[A], ordered: (A,A) => Boolean): Boolean = {
+    val len = as.length
+    def check_all(i: Int) : Boolean = 
+      if (i >= len) true
+      else ordered(as(i-1), as(i)) && check_all(i+1)
+
+    check_all(1)
+  }
+}
