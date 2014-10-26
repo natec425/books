33e921080d59d10e4f99cb146bbd817783ca3704
Nate Clark <natec425@gmail.com>
2014-10-26 14:34:46 -0500

    Added Tree definition from book. Implemented size (3.25)

--- fun_prog_scala/chapter3.scala
+++ fun_prog_scala/chapter3.scala
@@ -103,3 +103,14 @@ object List {
     case Cons(_, tl) => hasPrefix(sup,sub) || hasSubsequence(tl, sub)
   }
 }
+
+sealed trait Tree[+A]
+case class Leaf[A](value: A) extends Tree[A]
+case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
+
+object Tree {
+  def size[A](t: Tree[A]): Int = t match {
+    case Leaf(_) => 1
+    case Branch(left, right) => 1 + size(left) + size(right)
+  }
+}
