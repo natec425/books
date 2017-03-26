77d04d2aedfecc84ff454bef81d1a25c411f24c2
Nate Clark <natec425@gmail.com>
2017-03-26 09:47:45 -0500

    Exercise 4.2

--- fun_prog_scala/src/main/scala/chapter4.scala
+++ fun_prog_scala/src/main/scala/chapter4.scala
@@ -1,25 +1,34 @@
-sealed trait Option[+A] {
-  def map[B](f: A => B): Option[B]
-  def flatMap[B](f: A => Option[B]): Option[B]
-  def getOrElse[B >: A](default: => B): B
-  def orElse[B >: A](ob: => Option[B]): Option[B]
-  def filter(f: A => Boolean): Option[A]
-}
+object Chapter4 {
+    sealed trait Option[+A] {
+      def map[B](f: A => B): Option[B]
+      def flatMap[B](f: A => Option[B]): Option[B]
+      def getOrElse[B >: A](default: => B): B
+      def orElse[B >: A](ob: => Option[B]): Option[B]
+      def filter(f: A => Boolean): Option[A]
+    }
 
-case class Some[+A](get: A) extends Option[A] {
-  def map[B](f: A => B): Option[B] = Some(f(get))
-  def getOrElse[B >: A](default: => B): B = get
+    case class Some[+A](get: A) extends Option[A] {
+      def map[B](f: A => B): Option[B] = Some(f(get))
+      def getOrElse[B >: A](default: => B): B = get
 
-  def flatMap[B](f: A => Option[B]): Option[B] = f(get)
-  def orElse[B >: A](ob: => Option[B]): Option[B] = this
-  def filter(f: A => Boolean): Option[A] =
-    if f(get) then this else None
-}
+      def flatMap[B](f: A => Option[B]): Option[B] = f(get)
+      def orElse[B >: A](ob: => Option[B]): Option[B] = this
+      def filter(f: A => Boolean): Option[A] =
+        if f(get) then this else None
+    }
 
-case object None extends Option[Nothing] {
-  def map[B](f: Nothing => B) = None
-  def flatMap[B](f: Nothing => Option[B]) = None
-  def getOrElse[B](default: => B) = default
-  def orElse[B](ob: => Option[B]) = ob
-  def filter(f: Nothing => Boolean) = None
+    case object None extends Option[Nothing] {
+      def map[B](f: Nothing => B) = None
+      def flatMap[B](f: Nothing => Option[B]) = None
+      def getOrElse[B](default: => B) = default
+      def orElse[B](ob: => Option[B]) = ob
+      def filter(f: Nothing => Boolean) = None
+    }
+
+    def mean(xs: Seq[Double]): Option[Double] =
+      if (xs.isEmpty) None
+      else Some(xs.sum / xs.length)
+
+    def variance(xs: Seq[Double]): Option[Double] =
+      mean(xs).flatMap(m => mean(xs.map(x => math.pow(x - m, 2))))
 }
