6522b9c2b4bb55bd5b6b25af351a6a3b8eb3c751
Nate Clark <natec425@gmail.com>
2017-03-26 09:22:10 -0500

    Add exercise 4.1

---
+++ fun_prog_scala/src/main/scala/chapter4.scala
@@ -0,0 +1,25 @@
+sealed trait Option[+A] {
+  def map[B](f: A => B): Option[B]
+  def flatMap[B](f: A => Option[B]): Option[B]
+  def getOrElse[B >: A](default: => B): B
+  def orElse[B >: A](ob: => Option[B]): Option[B]
+  def filter(f: A => Boolean): Option[A]
+}
+
+case class Some[+A](get: A) extends Option[A] {
+  def map[B](f: A => B): Option[B] = Some(f(get))
+  def getOrElse[B >: A](default: => B): B = get
+
+  def flatMap[B](f: A => Option[B]): Option[B] = f(get)
+  def orElse[B >: A](ob: => Option[B]): Option[B] = this
+  def filter(f: A => Boolean): Option[A] =
+    if f(get) then this else None
+}
+
+case object None extends Option[Nothing] {
+  def map[B](f: Nothing => B) = None
+  def flatMap[B](f: Nothing => Option[B]) = None
+  def getOrElse[B](default: => B) = default
+  def orElse[B](ob: => Option[B]) = ob
+  def filter(f: Nothing => Boolean) = None
+}
